32163197e7bafb17f3818b72b5b5de3477caf806
Jarad Niemi <niemi@iastate.edu>
2019-12-19 21:41:36 -0800

    Fix typos

--- data-raw/level_rewards.R
+++ data-raw/level_rewards.R
@@ -1,3 +1,7 @@
-level_rewards <- readr::read_csv("level_rewards.csv")
+level_rewards <- readr::read_csv("level_rewards.csv",
+                                 col_types = cols(
+                                   battle = col_character(),
+                                   reward = col_character()
+                                 ))
 
 usethis::use_data(level_rewards, overwrite = TRUE)
