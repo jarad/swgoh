821e9ee23d5f298ee91dae13e9ca43add24d500a
Jarad Niemi <niemi@iastate.edu>
2019-12-20 17:35:33 -0800

    Add add script and documentation for `reward` data.frame

--- R/rewards.R
+++ R/rewards.R
@@ -1,11 +1,15 @@
 #' Rewards
 #' 
-#' Rewards that are randomly awarded
+#' Rewards received from competing in battles
 #' 
 #' @format A data frame with 3 columns:
 #' \describe{
-#'   \item{battleID}{unique battle identifier}
-#'   \item{reward}{name of reward}
-#'   \item{count}{number received}
+#'   \item{battleID}{character, unique battle identifier}
+#'   \item{date}{Date, date of battle}
+#'   \item{userID}{integer, user identifier}
+#'   \item{battle}{character, name of the battle}
+#'   \item{simulated}{logical, whether or not the battle was simulated}
+#'   \item{n}{integer, the number to battle attempts}
+#'   \item{...}{remaining columns are reward names and the number of that reward received for that battle}
 #' }
 "rewards"
--- data-raw/rewards.R
+++ data-raw/rewards.R
@@ -1,46 +1,85 @@
 library("dplyr")
 library("tidyr")
+library("readr")
 
-my_read_csv = function(f, into) {
-  readr::read_csv(f, col_types = cols(
-    battleID = col_integer(),
-    reward   = col_character(),
-    count    = col_integer()
+# Create reward data set by joining battle and reward files and converting 
+# dataset to a wide data set with lots of zeros
+
+################################################################################
+# Battles
+
+read_battle_csv = function(f, into) {
+  readr::read_csv(f, 
+                  col_types = readr::cols(
+                    battleID = readr::col_integer(),
+                    userID   = readr::col_integer(),
+                    battle   = readr::col_character(),
+                    n_sims   = readr::col_integer()
   )) %>%
     dplyr::mutate(file=f) %>%
     tidyr::separate(file, into) 
 }
 
-read_dir = function(path, pattern, into) {
+read_battle_dir = function(path, pattern, into) {
   files = list.files(path = path,
                      pattern = pattern,
                      recursive = TRUE,
                      full.names = TRUE)
-  plyr::ldply(files, my_read_csv, into = into)
+  plyr::ldply(files, read_battle_csv, into = into)
 }
 
-static <- c("Player XP", "Credits","Ally Point","Cantina Battle Tokens",
-            "Ship Building Materials","Sim Tickets",
-            "T1 Training Droid","T2 Training Droid","T3 Training Droid",
-            "T1 Enhancement Droid")
-
-
-########################################################################
-  
-rewards <- read_dir(path    = "rewards",
-                    pattern = "*.csv",
-                    into    = c("rewards","date","extension")) %>%
+battles <- read_battle_dir(path    = "battles",
+                           pattern = "*.csv",
+                           into    = c("rewards","date","extension")) %>%
   
   dplyr::mutate(
-    battleID = paste0(gsub("r", "b", date), "-",
-                      formatC(battleID, width = 3, flag = "0"))) %>% 
+    date      = gsub("b", "", date), 
+    battleID  = paste0(date, "-", formatC(battleID, width = 3, flag = "0")),
+    date      = as.Date(date, format = "%Y%m%d"),
+    simulated = ifelse(n_sims == 0, FALSE, TRUE),
+    n         = n_sims + (n_sims == 0)) %>%
   
-  dplyr::select(battleID, reward, count)
+  select(date, battleID, userID, battle, simulated, n)
 
-rewards_static <- rewards %>% filter(   reward %in% static  )
-rewards        <- rewards %>% filter( !(reward %in% static) )
+
+
+################################################################################
+# Rewards
+
+read_reward_csv = function(f, into) {
+  readr::read_csv(f, 
+                  col_types = readr::cols(
+                    battleID = readr::col_integer(),
+                    reward   = readr::col_character(),
+                    count    = readr::col_integer()
+  )) %>%
+    dplyr::mutate(file=f) %>%
+    tidyr::separate(file, into) 
+}
+
+read_reward_dir = function(path, pattern, into) {
+  files = list.files(path = path,
+                     pattern = pattern,
+                     recursive = TRUE,
+                     full.names = TRUE)
+  plyr::ldply(files, read_reward_csv, into = into)
+}
+
+rewards <- read_reward_dir(path    = "rewards",
+                           pattern = "*.csv",
+                           into    = c("rewards","date","extension")) %>%
+  
+  dplyr::mutate(
+    date = gsub("r", "", date),
+    battleID = paste0(date, "-", formatC(battleID, width = 3, flag = "0"))) %>% 
+  
+  select(battleID, reward, count) %>%
+  
+  dplyr::full_join(battles, by="battleID") %>%
+  
+  dplyr::mutate(count = as.integer(count)) %>%
+  tidyr::spread(reward, count, fill=0)
   
 
-usethis::use_data(rewards,        overwrite = TRUE)
-usethis::use_data(rewards_static, overwrite = TRUE)
+usethis::use_data(rewards, overwrite = TRUE)
   
