96119806a042ea0c3ea078718ab55a28e966da5f
Jarad Niemi <niemi@iastate.edu>
2020-01-03 07:59:19 -0800

    Create monthly files for users data

--- data-raw/run_all.R
+++ data-raw/run_all.R
@@ -1,4 +1,9 @@
 # Prioritized by the least likely to change on top
+
 source("battle_rewards.R")
+
 source("users.R")
+
+source("battles.R")
 source("rewards.R")
+source("store.R")
--- data-raw/users.R
+++ data-raw/users.R
@@ -1,8 +1,33 @@
-users <- readr::read_csv("users.csv", 
-                         col_types = readr::cols(
-                           date = col_date(format = "%Y-%m-%d"),
-                           userID = col_integer(),
-                           level = col_integer()
-                         ))
+library("readr")
+library("dplyr")
+library("tidyr")
+
+col_types <- readr::cols(
+  date     = readr::col_date(format = "%Y-%m-%d"),
+  userID   = readr::col_integer(),
+  level    = readr::col_integer()
+)
+
+read_users_csv = function(f, into) {
+  readr::read_csv(f, col_types = col_types) %>%
+    dplyr::mutate(file=f) %>%
+    tidyr::separate(file, into) 
+}
+
+read_users_dir = function(path, pattern, into) {
+  files = list.files(path = path,
+                     pattern = pattern,
+                     recursive = TRUE,
+                     full.names = TRUE)
+  plyr::ldply(files, read_users_csv, into = into)
+}
+
+########################################################################
+
+users <- read_users_dir(path    = "users",
+                          pattern = "*.csv",
+                          into    = c("users","year","month","extension")) %>%
+  
+  select(date, userID, level)
 
 usethis::use_data(users, overwrite = TRUE)
