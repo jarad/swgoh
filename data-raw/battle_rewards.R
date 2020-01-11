06238a95e102ca90143ca4852903a6a323b0e54b
Jarad Niemi <niemi@iastate.edu>
2020-01-11 11:34:11 -0800

    Move battle_rewards into its own directory

--- data-raw/battle_rewards.R
+++ data-raw/battle_rewards.R
@@ -1,4 +1,4 @@
-battle_rewards <- readr::read_csv("battle_rewards.csv",
+battle_rewards <- readr::read_csv("battle_rewards/battle_rewards.csv",
                                   col_types = cols(
                                     battle = col_character(),
                                     reward = col_character()
