1a63835d776a85b5580eafbce97487e6e8d7f992
Jarad Niemi <niemi@iastate.edu>
2020-03-03 06:03:20 -0800

    Add galactic power column

--- R/users.R
+++ R/users.R
@@ -1,11 +1,12 @@
 #' Users
 #' 
-#' Level information for the three users for which data were taken
+#' Daily Level and galactic power for users
 #' 
-#' @format A data frame with 3 columns:
+#' @format A data frame with 4 columns:
 #' \describe{
 #'   \item{date}{Date, date of battle}
-#'   \item{userID}{integer, user identifier}
-#'   \item{level}{integer, player level (1-85)}
+#'   \item{userID}{character, user identifier}
+#'   \item{level}{integer, player's level (1-85)}
+#'   \item{power}{integer, player's galactic power}
 #' }
 "users"
