1cf00ee5ac108842a6632f74f6ac7074b3ffcc1c
Jarad Niemi <niemi@iastate.edu>
2020-09-28 20:07:45 -0500

    Use Poisson model rather than binomial

--- inst/shiny/dropRates/rewards.R
+++ inst/shiny/dropRates/rewards.R
@@ -7,24 +7,35 @@ rewards <- swgoh::rewards %>%
   dplyr::group_by(battle, reward, n_sims) %>%
   dplyr::summarize(
     n = sum(attempts) + sum(!simulated),
-    y = sum(count)) %>%
+    y = sum(count),
+    .groups = "keep") %>%
   dplyr::ungroup() %>%
   
   left_join(swgoh::reward_details, by = "reward") %>%
   
-  # Binomial model with Jeffreys prior
+  # Poisson model with Ga(0.5, 0.5) prior
   dplyr::mutate(
+    # a = 0.5 + y,
+    # b = 0.5 + n - y,
+    # p  = y / n,
+    # 
+    # lb = qbeta(.025, a, b),
+    # ub = qbeta(.975, a, b),
+    # 
+    # lb = ifelse(p==0, 0, lb),
+    # ub = ifelse(p==0, qbeta(.95, a, b), ub),
+    # 
+    # lb = ifelse(p==1, qbeta(.05, a, b), lb),
+    # ub = ifelse(p==1, 1, ub),
+    
     a = 0.5 + y,
-    b = 0.5 + n - y,
-    p  = y / n,
+    b = 0.5 + n,
+    rate  = a/b,
     
-    lb = qbeta(.025, a, b),
-    ub = qbeta(.975, a, b),
+    lb = qgamma(.025, a, b),
+    ub = qgamma(.975, a, b),
     
-    lb = ifelse(p==0, 0, lb),
-    ub = ifelse(p==0, qbeta(.95, a, b), ub),
-    
-    lb = ifelse(p==1, qbeta(.05, a, b), lb),
-    ub = ifelse(p==1, 1, ub),
+    lb = ifelse(y==0, 0, lb),
+    ub = ifelse(y==0, qgamma(.95, a, b), ub),
     
     n_sims = as.integer(n_sims)) 
