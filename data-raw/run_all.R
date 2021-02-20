ce44fdc0f7c724b67a24884a68b43753bfb52969
Jarad Niemi <niemi@iastate.edu>
2021-02-20 06:51:51 -0600

    Move data files to new repository
    https://github.com/jarad/swgoh-data
    
    closes #9

--- data-raw/run_all.R
+++
@@ -1,8 +0,0 @@
-# Prioritized by the least likely to change on top
-
-source("battle_details.R")
-source("reward_details.R")
-
-source("users.R")
-source("rewards.R")  # runs source("battles.R")
-# source("bronzium.R")
