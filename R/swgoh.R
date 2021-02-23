c92279e856e880f699ea379691601130e309eb96
Jarad Niemi <niemi@iastate.edu>
2021-02-23 14:16:37 -0600

    Add package documentation

---
+++ R/swgoh.R
@@ -0,0 +1,16 @@
+#' swgoh: An R package for Star Wars Galaxy of Heroes data
+#' 
+#' This package contains data on battles fought and rewards obtained while 
+#' playing the mobile game Star Wars Galaxy of Heroes. 
+#' 
+#' @section Data:
+#' 
+#' \code{\link{rewards}}: this data set contains the rewards received from 
+#' battles including details about the date the battle was fought, the user
+#' who fought the battle, whether or not the battle was simulated, and, if 
+#' simulated, the number of simulations run. 
+#' 
+#' @docType package
+#' @name swgoh
+#' 
+NULL
